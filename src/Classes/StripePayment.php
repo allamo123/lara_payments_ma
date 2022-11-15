3bc1e4898d7fc76e5b815c14492998043d4051e9
mohamed allam <55511778+allamo123@users.noreply.github.com>
2022-11-15 20:02:29 +0200

    Update Stripe

--- src/Classes/StripePayment.php
+++ src/Classes/StripePayment.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Cache;
 use Ma\Payments\Interfaces\PaymentInterface;
 use Stripe\StripeClient;
 
@@ -39,10 +40,15 @@ class StripePayment extends BaseController implements PaymentInterface
     {
         $this->setPassedVariablesToGlobal($amount,$user_id,$user_first_name,$user_last_name,$user_email,$user_phone,$source, $card);
 
+        $required_fields = ['card'];
+
+        $this->checkRequiredFields($required_fields, 'Stripe', func_get_args());
+
         $stripe = new StripeClient($this->stripe_api_secret);
 
         $token = $stripe->tokens->create([
             'card' => [
+                'name' => $this->card['card_holder_name'],
                 'number' =>  $this->card['card_number'],
                 'exp_month' => $this->card['ex_month'],
                 'exp_year' => $this->card['ex_year'],
@@ -50,20 +56,47 @@ class StripePayment extends BaseController implements PaymentInterface
             ],
         ]);
 
-        $stripe->charges->create([
+        $payment = $stripe->charges->create([
             'amount' => $this->amount * 100,
             'currency' =>  $this->stripe_currency,
             'source' => $token['id'],
             'description' => 'My First Test Charge (created for API docs at https://www.stripe.com/docs/api)',
         ]);
+
+        if ($payment['captured'] === true && $payment['paid'] === true && $payment['status'] === 'succeeded') {
+
+            Cache::forever('payment_id', $payment['id']);
+
+            return [
+                'success' => true,
+                'verify_route' => $this->verify_route,
+                'payment_id' => $payment['id']
+            ];
+        }
+
+        // return false;
     }
 
-    /**
-     * @param Request $request
-     * @return array
-     */
-    public function verify(Request $request): array
+    public function verify(Request $request)
     {
-        #
+        $payment_id = Cache::get('payment_id');
+
+        Cache::forget('payment_id');
+
+        $stripe = new \Stripe\StripeClient(
+            $this->stripe_api_secret
+        );
+
+        $transaction = $stripe->charges->retrieve(
+                $payment_id,
+                []
+        );
+
+        if ($transaction['captured'] && $transaction['paid'] && $transaction['status'] === 'succeeded') {
+            return [
+                'success' => true,
+                'transaction' => $transaction
+            ];
+        }
     }
 }

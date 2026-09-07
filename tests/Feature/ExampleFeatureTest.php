f47afc0c30c4866f39bd576b08729cb799a8c02b
allamo123 <m.pro22.allam@gmail.com>
2026-09-07 21:26:12 +0300

    fix testing enviorment implementation

---
+++ tests/Feature/ExampleFeatureTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleFeatureTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}

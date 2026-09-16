c256315d05dc454e48d58212025eb0537b3b856d
allamo123 <m.pro22.allam@gmail.com>
2026-09-16 08:18:14 +0300

    update normal payment webhook handler

--- src/Gateways/Paymob/Services/PaymobWebhookHandler.php
+++ src/Gateways/Paymob/Services/PaymobWebhookHandler.php
@@ -2,40 +2,74 @@
 
 namespace Ma\Payment\Gateways\Paymob\Services;
 
+use Illuminate\Support\Facades\Log;
 use RuntimeException;
 
 final class PaymobWebhookHandler
 {
-    public function handle(array $callbackResponse)
+    public function handle(array $proccessedData): array
+    {
+        Log::info('payload_show', [
+            'issue_daata' =>  $proccessedData,
+        ]);
+
+        $data = $proccessedData;
+
+        $type = $data['type'] ?? null;
+
+        $webhookData = match ($type) {
+           'TRANSACTION'  => $this->verifyHMAC($data),
+           'TOKEN'  => $this->cardDetails($data['obj']),
+            default => throw new RuntimeException('type not exist at webhook handler'),
+        };
+
+        return [
+            'type' => $type,
+            'source' => 'payment',
+            'data' => $webhookData,
+        ];
+    }
+
+    public function verifyHMAC(array $transaction): array
     {
         $string = 
-            $callbackResponse['amount_cents'] . 
-            $callbackResponse['created_at'] . 
-            $callbackResponse['currency'] . 
-            $callbackResponse['error_occured'] . 
-            $callbackResponse['has_parent_transaction'] . 
-            $callbackResponse['id'] . 
-            $callbackResponse['integration_id'] . 
-            $callbackResponse['is_3d_secure'] . 
-            $callbackResponse['is_auth'] . 
-            $callbackResponse['is_capture'] . 
-            $callbackResponse['is_refunded'] . 
-            $callbackResponse['is_standalone_payment'] . 
-            $callbackResponse['is_voided'] . 
-            $callbackResponse['order'] . 
-            $callbackResponse['owner'] . 
-            $callbackResponse['pending'] . 
-            $callbackResponse['source_data_pan'] . 
-            $callbackResponse['source_data_sub_type'] . 
-            $callbackResponse['source_data_type'] . 
-            $callbackResponse['success'];
+            $transaction['obj']['amount_cents'] . 
+            $transaction['obj']['created_at'] . 
+            $transaction['obj']['currency'] . 
+            ($transaction['obj']['error_occured'] ? 'true' : 'false') . 
+            ($transaction['obj']['has_parent_transaction'] ? 'true' : 'false') . 
+            $transaction['obj']['id'] . 
+            $transaction['obj']['integration_id'] . 
+            ($transaction['obj']['is_3d_secure'] ? 'true' : 'false') . 
+            ($transaction['obj']['is_auth'] ? 'true' : 'false') . 
+            ($transaction['obj']['is_capture'] ? 'true' : 'false') . 
+            ($transaction['obj']['is_refunded'] ? 'true' : 'false') . 
+            ($transaction['obj']['is_standalone_payment'] ? 'true' : 'false') . 
+            ($transaction['obj']['is_voided'] ? 'true' : 'false') . 
+            $transaction['obj']['order']['id'] . 
+            $transaction['obj']['owner'] . 
+            ($transaction['obj']['pending'] ? 'true' : 'false') . 
+            $transaction['obj']['source_data']['pan'] . 
+            $transaction['obj']['source_data']['sub_type'] . 
+            $transaction['obj']['source_data']['type'] . 
+            ($transaction['obj']['success'] ? 'true' : 'false');
 
-        $hmac = hash_hmac('sha512', $string, config('ma-payment.PAYMOB_HMAC'));
+        $calculatedHmac = hash_hmac('sha512', $string, config('ma-payment.PAYMOB_HMAC'));
 
-        if (!hash_equals($hmac, $callbackResponse['hmac'])) {
+        if (!hash_equals($calculatedHmac, $transaction['hmac'])) {
             throw new RuntimeException(
                 'Invalid Paymob transaction HMAC.'
             );
         }
+
+        return [
+            'hmac' => true,
+            'transaction' => $transaction
+        ];
+    }
+
+    public function cardDetails(array $cardDetails): array
+    {
+        return $cardDetails;
     }
 }
